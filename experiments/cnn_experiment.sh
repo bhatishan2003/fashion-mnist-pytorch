49bcb5ab990496a9b40de8c0c32a374d47382914
Ishan Bhat <ishan2003bhat@gmail.com>
2024-12-30 19:28:15 +0000

    chore: experiment lr, optim , cnn

---
+++ experiments/cnn_experiment.sh
@@ -0,0 +1,3 @@
+python main.py --mode train --learning_rate 0.01 --optimizer sgd --seed 0 --num_epochs 25 --use_wandb --model_type cnn
+python main.py --mode train --learning_rate 0.01 --optimizer sgd --seed 1 --num_epochs 25 --use_wandb --model_type cnn
+python main.py --mode train --learning_rate 0.01 --optimizer sgd --seed 2 --num_epochs 25 --use_wandb --model_type cnn
---
+++ experiments/lr_experiment.sh
@@ -0,0 +1,9 @@
+python main.py --mode train --learning_rate 0.1 --optimizer sgd --seed 0 --num_epochs 25 --use_wandb
+python main.py --mode train --learning_rate 0.1 --optimizer sgd --seed 1 --num_epochs 25 --use_wandb
+python main.py --mode train --learning_rate 0.1 --optimizer sgd --seed 2 --num_epochs 25 --use_wandb
+python main.py --mode train --learning_rate 0.01 --optimizer sgd --seed 0 --num_epochs 25 --use_wandb
+python main.py --mode train --learning_rate 0.01 --optimizer sgd --seed 1 --num_epochs 25 --use_wandb
+python main.py --mode train --learning_rate 0.01 --optimizer sgd --seed 2 --num_epochs 25 --use_wandb
+python main.py --mode train --learning_rate 0.001 --optimizer sgd --seed 0 --num_epochs 25 --use_wandb
+python main.py --mode train --learning_rate 0.001 --optimizer sgd --seed 1 --num_epochs 25 --use_wandb
+python main.py --mode train --learning_rate 0.001 --optimizer sgd --seed 2 --num_epochs 25 --use_wandb
